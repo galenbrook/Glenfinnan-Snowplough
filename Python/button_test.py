85f1a77369b62000c11da7f1c2b39ffde2f896e5
Galen Brook <galenbrook@gmail.com>
2025-04-03 16:01:48 +0100

    Added script to light LED on button press

--- Python/button_test.py
+++ Python/button_test.py
@@ -1,4 +1,4 @@
-from gpiozero import Button
+from gpiozero import Button, LED
 from signal import pause
 
 count = 0
@@ -7,8 +7,14 @@ def confirm_button_press():
     global count
     count+=1
     print("Button was pressed " + str(count) + " times!")
+    if (count%2):
+        led.on()
+    else:
+        led.off()
 
-button = Button(pin=21, pull_up=False, bounce_time=0.01)
+
+button = Button(pin=18, pull_up=True, bounce_time=0.005)
+led = LED(pin=20)
 
 button.when_pressed = confirm_button_press
 
